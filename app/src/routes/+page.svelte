1e11d5edfdf27697e18eb942dd85e070483a1a2e
NikoKhab <nmangori.1@gmail.com>
2025-03-24 23:45:31 +0400

    api fetched from the server

---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+const { data } = $props()
+let questions = $state(JSON.stringify(data))
+console.log(questions)
+//TODO: there will be a variable currentQuestion that will display current question (that is obvius, i am just dumb)
+</script>
+
+<main>
+{questions}
+</main>/
---
+++ app/src/routes/+page.ts
@@ -0,0 +1,15 @@
+export const load = async () => {
+  try {
+    const res = await fetch(
+      //whoever uses my key the first is gay:
+      "https://quizapi.io/api/v1/questions?apiKey=pc3e7n5JABLyiczbKvcMNKJAieCD5cGEtWP7MlR1&category=vuejs&difficulty=Medium&limit=10&tags=VueJS"
+    );
+    const quiz = await res.json();
+
+    console.log(quiz);
+    return { questions: quiz };
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
+    return { questions: [] };
+  }
+};
